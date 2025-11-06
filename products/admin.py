ddd6950194732738d9f22a2508bc5d7c43ae5af5
FaresBza <fares.bouzaria@crea-inseec.com>
2025-11-06 14:41:26 +0100

    Add review model, update admin model and generate migrations

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Product
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "created_at")
-    search_fields = ("name",)
+from .models import Product, Review
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "user", "rating", "created_at")
+    search_fields = ("product__name", "user__username")
